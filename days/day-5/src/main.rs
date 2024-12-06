3ded90912187beb7031f27f1f28897515f017618
Pink Garrett <pink@thepinkhacker.com>
2024-12-06 18:06:56 -0500

    Day 5 WIP 2

--- days/day-5/src/main.rs
+++ days/day-5/src/main.rs
@@ -2,19 +2,29 @@ use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../input");
 
-fn parse_input(raw: &str) -> (HashMap<u8, Vec<u8>>, Vec<Vec<u8>>) {
-    let (orders, pages) = raw.split_once("\n\n").unwrap();
+type Table = HashMap<u8, Vec<u8>>;
 
+fn parse_orders(orders: &str) -> (Table, Table) {
     let orders = orders.lines().map(|line| {
         let (before, after) = line.split_once('|').unwrap();
         (before.parse::<u8>().unwrap(), after.parse::<u8>().unwrap())
     });
 
-    let mut order_table = HashMap::<u8, Vec<u8>>::new();
+    let mut before_table = HashMap::<u8, Vec<u8>>::new();
+    let mut after_table = HashMap::<u8, Vec<u8>>::new();
 
     for (before, after) in orders {
-        order_table.entry(before).or_default().push(after);
+        before_table.entry(after).or_default().push(before);
+        after_table.entry(before).or_default().push(after);
     }
+
+    (before_table, after_table)
+}
+
+fn parse_input(raw: &str) -> (Table, Table, Vec<Vec<u8>>) {
+    let (orders, pages) = raw.split_once("\n\n").unwrap();
+
+    let (before_table, after_table) = parse_orders(orders);
 
     let pages = pages
         .lines()
@@ -26,56 +36,84 @@ fn parse_input(raw: &str) -> (HashMap<u8, Vec<u8>>, Vec<Vec<u8>>) {
         })
         .collect();
 
-    (order_table, pages)
+    (before_table, after_table, pages)
 }
 
-fn seek<'a, I>(after_values: &[u8], page: &I) -> bool
-where
-    I: Iterator<Item = &'a u8>,
-{
-    for after in after_values {
-        let x = page.by_ref().find(|x| *x == after);
+fn seek(values: &[u8], page: &[u8]) -> bool {
+    for value in values {
+        if !page.contains(value) {
+            return false;
+        }
     }
 
-    false
+    true
 }
 
-fn part_one(orders: &HashMap<u8, Vec<u8>>, pages: &[Vec<u8>]) -> u32 {
-    let mut output = 0;
-
-    'page: for page in pages {
-        let mut page_iter = page.iter();
-
-        while let Some(before) = page_iter.next() {
-            if let Some(after) = orders.get(before) {
-                if !seek(after, &page_iter) {
-                    dbg!(before, after);
-                    continue 'page;
-                }
+fn check_page(before_table: &Table, after_table: &Table, page: &[u8]) -> Option<u8> {
+    for (i, value) in page.iter().enumerate() {
+        if let Some(before) = before_table.get(value) {
+            if seek(before, &page[i + 1..]) {
+                // Before value was found after value
+                return None;
+            } else {
+                dbg!("BEFORE", value, before, page);
             }
         }
 
-        let middle_value = page[page.len() / 2];
-        dbg!(middle_value);
-        output += middle_value as u32;
+        if let Some(after) = after_table.get(value) {
+            if seek(after, &page[..i]) {
+                // After value was found before value
+                return None;
+            }
+        }
     }
 
-    output
+    Some(page[page.len() / 2])
+}
+
+fn part_one(before_table: &Table, after_table: &Table, pages: &[Vec<u8>]) -> u32 {
+    pages
+        .iter()
+        .filter_map(|page| check_page(before_table, after_table, page))
+        .map(u32::from)
+        .sum()
 }
 
 fn main() {
-    let (orders, pages) = parse_input(INPUT);
+    let (before_table, after_table, pages) = parse_input(INPUT);
 
     println!("=== Day 5 ===");
 
     println!();
     println!("Part One:");
-    println!("{}", part_one(&orders, &pages));
+    println!("{}", part_one(&before_table, &after_table, &pages));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const EXAMPLE_ONE_ORDERS: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13";
 
     const EXAMPLE_ONE: &str = "47|53
 97|13
@@ -107,8 +145,58 @@ mod tests {
 97,13,75,29,47";
 
     #[test]
+    fn example_1_0() {
+        let (before_table, after_table) = parse_orders(EXAMPLE_ONE_ORDERS);
+        let page = &[75, 47, 61, 53, 29];
+        assert_eq!(check_page(&before_table, &after_table, page), Some(61));
+    }
+
+    #[test]
+    fn example_1_1() {
+        let (before_table, after_table) = parse_orders(EXAMPLE_ONE_ORDERS);
+        let page = &[97, 61, 53, 29, 13];
+        assert_eq!(check_page(&before_table, &after_table, page), Some(53));
+    }
+
+    #[test]
+    fn example_1_2() {
+        let (before_table, after_table) = parse_orders(EXAMPLE_ONE_ORDERS);
+        let page = &[75, 29, 13];
+        assert_eq!(check_page(&before_table, &after_table, page), Some(29));
+    }
+
+    #[test]
+    fn example_1_3() {
+        let (before_table, after_table) = parse_orders(EXAMPLE_ONE_ORDERS);
+        let page = &[75, 97, 47, 61, 43];
+        assert_eq!(check_page(&before_table, &after_table, page), None);
+    }
+
+    #[test]
+    fn example_1_4() {
+        let (before_table, after_table) = parse_orders(EXAMPLE_ONE_ORDERS);
+        let page = &[61, 13, 29];
+        assert_eq!(check_page(&before_table, &after_table, page), None);
+    }
+
+    #[test]
+    fn example_1_5() {
+        let (before_table, after_table) = parse_orders(EXAMPLE_ONE_ORDERS);
+        let page = &[97, 13, 75, 29, 47];
+        assert_eq!(check_page(&before_table, &after_table, page), None);
+    }
+
+    #[test]
     fn example_1() {
-        let (orders, pages) = parse_input(EXAMPLE_ONE);
-        assert_eq!(part_one(&orders, &pages), 143);
+        let (before_table, after_table, pages) = parse_input(EXAMPLE_ONE);
+        assert_eq!(part_one(&before_table, &after_table, &pages), 143);
+    }
+
+    #[test]
+    fn part_one_final() {
+        let (before_table, after_table, pages) = parse_input(INPUT);
+        // 46|62
+        let test = &[vec![32, 99, 27, 62, 48, 64, 46, 98, 14]];
+        assert_eq!(part_one(&before_table, &after_table, test), 0);
     }
 }
