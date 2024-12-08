11db3d4b007b4b63801e140174e39cf2093d6a19
Pink Garrett <pink@thepinkhacker.com>
2024-12-08 18:46:38 -0500

    Day 5 part two

--- days/day-5/src/main.rs
+++ days/day-5/src/main.rs
@@ -39,24 +39,53 @@ fn parse_input(raw: &str) -> (Table, Vec<Vec<u8>>) {
     (after_table, pages)
 }
 
-fn check_page(after_table: &Table, page: &[u8]) -> Option<u8> {
-    for pages in page.iter().combinations(2) {
+fn find_issue(after_table: &Table, page: &[u8]) -> Option<(usize, usize)> {
+    for pages in page.iter().enumerate().combinations(2) {
+        let (page_a_index, page_a) = pages[0];
+        let (page_b_index, page_b) = pages[1];
+
         if after_table
-            .get(pages[1])
-            .map(|values| values.contains(pages[0]))
+            .get(page_b)
+            .map(|values| values.contains(page_a))
             .unwrap_or_default()
         {
-            return None;
+            return Some((page_a_index, page_b_index));
         }
     }
 
-    Some(page[page.len() / 2])
+    None
+}
+
+fn middle_check(after_table: &Table, page: &[u8]) -> Option<u8> {
+    if find_issue(after_table, page).is_some() {
+        None
+    } else {
+        Some(page[page.len() / 2])
+    }
+}
+
+fn reorder(after_table: &Table, mut page: Vec<u8>) -> Vec<u8> {
+    while let Some((index_a, index_b)) = find_issue(after_table, &page) {
+        page.swap(index_a, index_b);
+    }
+
+    page
 }
 
 fn part_one(after_table: &Table, pages: &[Vec<u8>]) -> u32 {
     pages
         .iter()
-        .filter_map(|page| check_page(after_table, page))
+        .filter_map(|page| middle_check(after_table, page))
+        .map(u32::from)
+        .sum()
+}
+
+fn part_two(after_table: &Table, pages: Vec<Vec<u8>>) -> u32 {
+    pages
+        .into_iter()
+        .filter(|page| find_issue(after_table, page).is_some())
+        .map(|page| reorder(after_table, page))
+        .map(|page| page[page.len() / 2])
         .map(u32::from)
         .sum()
 }
@@ -69,6 +98,10 @@ fn main() {
     println!();
     println!("Part One:");
     println!("{}", part_one(&after_table, &pages));
+
+    println!();
+    println!("Part Two:");
+    println!("{}", part_two(&after_table, pages));
 }
 
 #[cfg(test)]
@@ -130,42 +163,42 @@ mod tests {
     fn example_1_0() {
         let after_table = parse_orders(EXAMPLE_ONE_ORDERS);
         let page = &[75, 47, 61, 53, 29];
-        assert_eq!(check_page(&after_table, page), Some(61));
+        assert_eq!(middle_check(&after_table, page), Some(61));
     }
 
     #[test]
     fn example_1_1() {
         let after_table = parse_orders(EXAMPLE_ONE_ORDERS);
         let page = &[97, 61, 53, 29, 13];
-        assert_eq!(check_page(&after_table, page), Some(53));
+        assert_eq!(middle_check(&after_table, page), Some(53));
     }
 
     #[test]
     fn example_1_2() {
         let after_table = parse_orders(EXAMPLE_ONE_ORDERS);
         let page = &[75, 29, 13];
-        assert_eq!(check_page(&after_table, page), Some(29));
+        assert_eq!(middle_check(&after_table, page), Some(29));
     }
 
     #[test]
     fn example_1_3() {
         let after_table = parse_orders(EXAMPLE_ONE_ORDERS);
         let page = &[75, 97, 47, 61, 43];
-        assert_eq!(check_page(&after_table, page), None);
+        assert_eq!(middle_check(&after_table, page), None);
     }
 
     #[test]
     fn example_1_4() {
         let after_table = parse_orders(EXAMPLE_ONE_ORDERS);
         let page = &[61, 13, 29];
-        assert_eq!(check_page(&after_table, page), None);
+        assert_eq!(middle_check(&after_table, page), None);
     }
 
     #[test]
     fn example_1_5() {
         let after_table = parse_orders(EXAMPLE_ONE_ORDERS);
         let page = &[97, 13, 75, 29, 47];
-        assert_eq!(check_page(&after_table, page), None);
+        assert_eq!(middle_check(&after_table, page), None);
     }
 
     #[test]
@@ -178,5 +211,11 @@ mod tests {
     fn part_one_final() {
         let (after_table, pages) = parse_input(INPUT);
         assert_eq!(part_one(&after_table, &pages), 7307);
+    }
+
+    #[test]
+    fn part_two_final() {
+        let (after_table, pages) = parse_input(INPUT);
+        assert_eq!(part_two(&after_table, pages), 4713);
     }
 }
